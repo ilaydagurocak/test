d1817ff59c7d3b30a63d8deb993e6b0e33d55b02
ilaydagurocak <128041640+ilaydagurocak@users.noreply.github.com>
2023-09-22 16:06:20 +0300

    Update kryptos-test.spec.ts

--- test/kryptos-test.spec.ts
+++ test/kryptos-test.spec.ts
@@ -1,16 +1,7 @@
-import { expect, test } from '@playwright/test';
-import { ElectronApplication, Page, _electron as electron } from '@playwright/test';
-import { flipFuses, FuseVersion, FuseV1Options } from '@electron/fuses'; // Import the fuse-flipping module
+import { expect, test, beforeAll, afterAll } from '@playwright/test';
+import { flipFuses, FuseVersion, FuseV1Options } from '@electron/fuses';
 
-let electronApp: ElectronApplication;
-let page: Page;
-const password = "Linos1140!";
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
-
-  process.env.CI = 'e2e';
-
+beforeAll(async () => {
   // Flip the fuses before launching Electron
   flipFuses(
     require('electron'), // Path to Electron
@@ -19,36 +10,21 @@ const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
       [FuseV1Options.RunAsNode]: false
     }
   );
+});
 
-  electronApp = await electron.launch({
+test('Uygulamaya login yap', async ({ electron }) => {
+  const password = "Linos1140!";
+
+  // Launch Electron
+  const electronApp = await electron.launch({
     executablePath: "C:\\Users\\vboxuser\\Downloads\\KryptosWDE_latest.exe"
   });
 
-  electronApp.on('window', async (page) => {
-    const filename = page.url()?.split('/').pop();
-    console.log(`Window opened: ${filename}`);
-
-    page.on('pageerror', (error) => {
-      console.error(error);
-    });
-
-    page.on('console', (msg) => {
-      console.log(msg.text());
-    });
-  });
-
-
-
-  await sleep(10000);
-  if (electronApp) {
-    await electronApp.close();
-  }
-
-
-test('Uygulamaya login yap', async () => {
-  test.setTimeout(120000);
+  // Wait for the login window to appear
   const timeout = 120000; 
   const startTime = Date.now();
+
+  let page;
 
   while ((Date.now() - startTime) <= timeout) {
     const newPage = await electronApp.waitForEvent('window');
@@ -64,14 +40,16 @@ test('Uygulamaya login yap', async () => {
     throw new Error('Timeout waiting for the login window');
   }
 
-  await sleep(30000);
+  // Simulate login
   await page.fill('[role="textbox"]', password);
   const buttons = await page.$$('button');
-  await buttons[1].click(); // 2. butona tıkla
+  await buttons[1].click(); // Click the second button
 
+  // Wait for the new window to appear
   const newWindow = await electronApp.waitForEvent('window');
   const newWindowTitle = await newWindow.title();
   expect(newWindowTitle).toBe('Kryptos Free');
   
-  page = newWindow;
+  // Close Electron
+  await electronApp.close();
 });
