0645fd7b65fbcda7a14de253666665c7d1102aca
ilaydagurocak <128041640+ilaydagurocak@users.noreply.github.com>
2023-09-22 09:47:28 +0300

    Add files via upload

---
+++ test/kryptos-test.spec.ts
@@ -0,0 +1,69 @@
+import { expect, test } from '@playwright/test';
+import { ElectronApplication, Page, _electron as electron } from '@playwright/test';
+
+let electronApp: ElectronApplication;
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+test.beforeAll(async () => {
+  process.env.CI = 'e2e'
+  electronApp = await electron.launch({
+    executablePath: "C:\\Program Files\\Kryptos\\Free\\KryptosWDE_latest.exe" // <-- Güncellenen kısım
+  });
+
+  electronApp.on('window', async (page) => {
+    const filename = page.url()?.split('/').pop()
+    console.log(`Window opened: ${filename}`)
+
+    page.on('pageerror', (error) => {
+      console.error(error)
+    })
+
+    page.on('console', (msg) => {
+      console.log(msg.text())
+    })
+  })
+
+})
+
+test.afterAll(async () => {
+  await sleep(10000);
+  await electronApp.close();
+})
+
+let page: Page;
+let password = "AAAAAAAA";
+
+test('Uygulamaya login yap', async () => {
+  test.setTimeout(120000);
+
+  const timeout = 120000; 
+  const startTime = Date.now();
+
+  if (page.title() != "Kryptos Free" ) {
+    while (true) {
+  
+      if (Date.now() - startTime > timeout) {
+        throw new Error('Timeout waiting for the login window');
+      }
+      const newPage = await electronApp.waitForEvent('window');
+      const title = await newPage.title();
+      
+      if (title == "Kryptos Free") {
+        page = newPage;
+        break;
+      }
+    }
+  }
+
+  await sleep(30000);
+
+  await page.getByRole('textbox').fill(password);
+  await page.$$eval('button', (buttons, index) => buttons[index].click(), 1);
+
+  const newWindow = await electronApp.waitForEvent('window');
+  const newWindowTitle = await newWindow.title();
+  expect(newWindowTitle).toBe('Kryptos Free');
+  
+  page = newWindow;
+});
